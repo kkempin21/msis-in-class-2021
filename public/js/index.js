a6157caf8be01be8d6508d448aab5790fb0702fc
kkempin21 <kylekempin1@gmail.com>
2021-09-21 22:53:23 -0400

    assignment3

--- public/js/index.js
+++ public/js/index.js
@@ -1,53 +1,36 @@
-
-const Offer = {
+const SomeApp = {
     data() {
       return {
-        "person": {},
-        "offers": [
-                {
-                    "id": 1,
-                    "name": "Jane Student",
-                    "offer": 100000,
-                    "bonus": 9000,
-                    "company": "EY",
-                    "offerDate": "2021-10-05"
-                },
-                {
-                    "id": 2,
-                    "name": "Jordan Student",
-                    "offer": 87000,
-                    "bonus": 3000,
-                    "company": "IU",
-                    "offerDate": "2021-09-25"
-                }
-            ]
-        }
+        result: {},
+        list: [5,6,7,8],
+        message: "Waiting ..."
+      }
     },
     computed: {
         prettyBirthday() {
-            return dayjs(this.person.dob.date)
-            .format('D MMM YYYY');
+            return dayjs(this.result.dob.date)
+            .format('D MMM YYYY')
         }
     },
     methods: {
         fetchUserData() {
+            //Method 1:
             fetch('https://randomuser.me/api/')
             .then(response => response.json())
-            .then((parsedJson) => {
-                console.log(parsedJson);
-                this.person = parsedJson.results[0]
+            .then((json) => {
+                console.log("Got json back:", json);
+                this.result = json.results[0];
                 console.log("C");
             })
-            .catch( err => {
-                console.error(err)
-            })
-
-            console.log("B");
+            .catch( (error) => {
+                console.error(error);
+            });
         }
     },
     created() {
         this.fetchUserData();
     }
+
   }
   
-Vue.createApp(Offer).mount('#offerApp');
+  Vue.createApp(SomeApp).mount('#someApp');
